29f539de833d5c5910a0a77e06e0273dd7693376
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 00:14:41 +0530

    UserCase1 done

--- PinCode.sh
+++ PinCode.sh
@@ -2,3 +2,15 @@
 
 echo "Welcome to Patterns PIN code"
 
+##UserCase1
+read -p "Enter pincode " Pin
+
+Pattern="[0-9]{6}"
+
+if [[ $Pin =~ $Pattern ]]
+then
+                echo "Valid"
+else
+                echo "Ïnvalid"
+fi
+
